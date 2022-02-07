5b7a3ba50ec270f3f8fea8fd298915b848b6ae8b
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-07 14:24:24 +0000

    Empty tests for WaterBottle

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+    WaterBottle bottle;
+
+    @Before
+    public void before() {
+        bottle = new Bottle();
+    }
+
+    @Test
+    public void volumeStartsAt100(){
+
+    }
+
+    @Test
+    public void canDrink(){
+
+    }
+
+    @Test
+    public void canEmpty(){
+
+    }
+
+    @Test
+    public void canFill(){
+
+    }
+
+}
