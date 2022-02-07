b218873e9b3c2b980392e021fa4e27bf6f67885d
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-07 14:11:05 +0000

    Empty tests for calculator

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @Before
+    public void before(){
+        calc = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+    }
+
+    @Test
+    public void canSubtract(){
+    }
+
+    @Test
+    public void canMultiply(){
+    }
+
+    @Test
+    public void canDivide(){
+    }
+
+}
