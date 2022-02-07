1abcc475d510ea44a63356af3671475c996e13af
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-07 14:27:22 +0000

    Added tests for waterbottle

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -12,22 +12,26 @@ public class WaterBottleTest {
 
     @Test
     public void volumeStartsAt100(){
-
+        assertEquals(100, bottle.getVolume());
     }
 
     @Test
     public void canDrink(){
-
+        bottle.drink();
+        assertEquals(90, bottle.getVolume());
     }
 
     @Test
     public void canEmpty(){
-
+        bottle.empty();
+        assertEquals(0, bottle.getVolume());
     }
 
     @Test
     public void canFill(){
-
+        bottle.empty();
+        bottle.fill
+        assertEquals(100, bottle.getVolume());
     }
 
 }
