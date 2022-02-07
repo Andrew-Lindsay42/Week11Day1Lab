6c6c483180825ffc581da1267d0f67e21dcca826
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-07 14:13:26 +0000

    Added tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     Calculator calc;
@@ -11,18 +12,22 @@ public class CalculatorTest {
 
     @Test
     public void canAdd(){
+        assertEquals(3, calc.add(1, 2));
     }
 
     @Test
     public void canSubtract(){
+        assertEquals(4, calc.subtract(10, 6));
     }
 
     @Test
     public void canMultiply(){
+        assertEquals(21, calc.multiply(7, 3));
     }
 
     @Test
     public void canDivide(){
+        assertEquals(4, calc.divide(8.0, 2.0));
     }
 
 }
